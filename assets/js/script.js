ea4f0ec4ae83043d03e08354a5617ec2fac6cfa7
garpeln <garpeln@163.com>
2024-07-13 11:45:29 +0800

    no message

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ gsap.to("#glassShine", {
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('nav');
 
-burger.addEventListener('click', e => {
-  burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed";
-  nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
-});
+// burger.addEventListener('click', e => {
+//   burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed";
+//   nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
+// });
