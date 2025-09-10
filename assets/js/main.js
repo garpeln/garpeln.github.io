544322883ff864facd4a24acc0d6a37104c55b28
garpeln <garpeln@aliyun.com>
2025-09-10 17:38:51 +0800

    2025-09-10 17:38:51

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,18 @@ $(document).ready(function() {
 
 });
 
+// 阅读进度条开始
+$(window).scroll(function() {
+    var a = $(window).scrollTop(),
+    c = $(document).height(),
+    b = $(window).height();
+    scrollPercent = a / (c - b) * 100;
+    scrollPercent = scrollPercent.toFixed(1);
+    $("#percentageCounter").css({
+        width: scrollPercent + "%"
+    });
+}).trigger("scroll");
+//阅读进度条结束
 
 
 
